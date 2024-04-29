637753b1639006d2058371e220d67b1c7e85f256
Robbie <robbie331@>
2024-04-29 16:51:19 +0900

    Level2의 7번 요구사항

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -10,62 +10,91 @@ public class App {
         Scanner sc = new Scanner(System.in);
 
         // 반복문을 돌면서 메모리 할당과 삭제를 반복하지 않도록하기 위해 반복문 밖에서 선언
-        int num1, num2;
+        int input, num1, num2;
         char operator;
         String repeat, firstRemove, inquiry;
         while (true) {
-            System.out.print("첫 번째 숫자를 입력하세요: ");
-            if (sc.hasNextInt()) { // sc.hasNextInt(): 정수값인지 확인하는 함수
-                num1 = sc.nextInt();
-            } else {
-                System.out.println("정수를 입력하세요.");
-                return;
+            System.out.println("-----------------------------");
+            System.out.println("1. 사칙연산 계산");
+            System.out.println("2. 원의 넓이 계산");
+            System.out.print("원하시는 번호를 입력해주세요: ");
+            input = sc.nextInt();
+            switch(input) {
+                case 1: // 사칙연산을 수행합니다.
+                    System.out.print("첫 번째 숫자를 입력하세요: ");
+                    if (sc.hasNextInt()) { // sc.hasNextInt(): 정수값인지 확인하는 함수
+                        num1 = sc.nextInt();
+                    } else {
+                        System.out.println("정수를 입력하세요.");
+                        return;
+                    }
+                    System.out.print("두 번째 숫자를 입력하세요: ");
+                    if (sc.hasNextInt()) {
+                        num2 = sc.nextInt();
+                    } else {
+                        System.out.println("정수를 입력하세요");
+                        return;
+                    }
+
+                    // 음수를 입력할 시 프로그램이 종료된다.
+                    if (num1 < 0 || num2 < 0) {
+                        System.out.println("양의 정수를 입력해주세요.");
+                        return;
+                    }
+
+                    System.out.print("사칙연산 기호를 입력하세요: ");
+                    // sc.next(): 입력받은 것을 String으로 받아온다.
+                    // charAt(int index): String의 String[index] 값을 가져와준다.
+                    operator = sc.next().charAt(0);
+
+                    try {
+                        calculator.calculate(num1, num2, operator);
+                    } catch(ArithmeticException e) {
+                        System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 올 수 없습니다.");
+                        continue;
+                    } catch(OperatorException e) {
+                        System.out.println(e.getMessage());
+                        continue;
+                    }
+
+                    System.out.println("결과: " + calculator.resultGetLast());
+
+                    // 가장 첫번째 값을 삭제한다.
+                    System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
+                    firstRemove = sc.next();
+                    if (firstRemove.equals("remove")) calculator.removeResult();
+
+                    System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
+                    inquiry = sc.next();
+                    if (inquiry.equals("inquiry")) calculator.inquiryResults();
+
+                    // exit를 입력하면 while 문을 빠져나가 프로그램 종료
+                    // exit외의 값이 입력되면 반복해서 계산한다.
+                    System.out.println("더 계산하시겠습니까? (exit 입력 시 종료) ");
+                    repeat = sc.next();
+                    if (repeat.equals("exit")) return;
+                    break;
+                case 2:
+                    System.out.print("원의 반지름을 입력해주세요: ");
+                    num1 = sc.nextInt();
+                    calculator.calculateCircleArea(num1);
+
+                    System.out.println("결과: " + calculator.circleGetLast());
+
+                    System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
+                    inquiry = sc.next();
+                    if (inquiry.equals("inquiry")) calculator.inquiryCircle();
+
+                    // exit를 입력하면 while 문을 빠져나가 프로그램 종료
+                    // exit외의 값이 입력되면 반복해서 계산한다.
+                    System.out.println("더 계산하시겠습니까? (exit 입력 시 종료) ");
+                    repeat = sc.next();
+                    if (repeat.equals("exit")) return;
+                    break;
+                default: System.out.println("1 or 2를 입력해주세요.");
             }
-            System.out.print("두 번째 숫자를 입력하세요: ");
-            if (sc.hasNextInt()) {
-                num2 = sc.nextInt();
-            } else {
-                System.out.println("정수를 입력하세요");
-                return;
-            }
 
-            // 음수를 입력할 시 프로그램이 종료된다.
-            if (num1 < 0 || num2 < 0) {
-                System.out.println("양의 정수를 입력해주세요.");
-                return;
-            }
 
-            System.out.print("사칙연산 기호를 입력하세요: ");
-            // sc.next(): 입력받은 것을 String으로 받아온다.
-            // charAt(int index): String의 String[index] 값을 가져와준다.
-            operator = sc.next().charAt(0);
-
-            try {
-                calculator.calculate(num1, num2, operator);
-            } catch(ArithmeticException e) {
-                System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 올 수 없습니다.");
-                continue;
-            } catch(OperatorException e) {
-                System.out.println(e.getMessage());
-                continue;
-            }
-
-            System.out.println("결과: " + calculator.resultGetLast());
-
-            // 가장 첫번째 값을 삭제한다.
-            System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
-            firstRemove = sc.next();
-            if (firstRemove.equals("remove")) calculator.removeResult();
-
-            System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
-            inquiry = sc.next();
-            if (inquiry.equals("inquiry")) calculator.inquiryResults();
-
-            // exit를 입력하면 while 문을 빠져나가 프로그램 종료
-            // exit외의 값이 입력되면 반복해서 계산한다.
-            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료) ");
-            repeat = sc.next();
-            if (repeat.equals("exit")) break;
         }
     }
 }
--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -3,60 +3,88 @@ package calculator;
 import java.util.LinkedList;
 
 public class Calculator {
-    private LinkedList<Double> result;
+
+    private static final double PI = 3.141592;
+
+    private LinkedList<Double> results;
+
+    // 원
+    private LinkedList<Double> circleArea;
 
     public Calculator() {
-        result = new LinkedList<Double>();
+        results = new LinkedList<Double>();
+        circleArea = new LinkedList<Double>();
     }
 
     public Calculator(int num1, int num2, char operator) throws ArithmeticException, OperatorException {
-        result = new LinkedList<Double>();
+        results = new LinkedList<Double>();
 
         switch(operator) {
-            case '+': result.add((double)num1 + num2); break;
-            case '-': result.add((double)num1 - num2); break;
-            case '*': result.add((double)num1 * num2); break;
-            case '/': result.add((double)num1 / num2); break;
+            case '+': results.add((double)num1 + num2); break;
+            case '-': results.add((double)num1 - num2); break;
+            case '*': results.add((double)num1 * num2); break;
+            case '/': results.add((double)num1 / num2); break;
             default: throw new OperatorException("잘못된 사칙연산 기호입니다. ");
         }
     }
 
     public void calculate(int num1, int num2, char operator) throws ArithmeticException, OperatorException {
         switch(operator) {
-            case '+': result.add((double)num1 + num2); break;
-            case '-': result.add((double)num1 - num2); break;
-            case '*': result.add((double)num1 * num2); break;
-            case '/': result.add((double)num1 / num2); break;
+            case '+': results.add((double)num1 + num2); break;
+            case '-': results.add((double)num1 - num2); break;
+            case '*': results.add((double)num1 * num2); break;
+            case '/': results.add((double)num1 / num2); break;
             default: throw new OperatorException("잘못된 사칙연산 기호입니다. ");
         }
     }
 
     public double resultGetter(int index) { // 해당 index의 값을 반환해준다.
-        return result.get(index);
+        return results.get(index);
     }
 
     public double resultGetLast() { // result list의 마지막 값을 반환해준다.
-        return result.getLast();
+        return results.getLast();
     }
 
     public void resultSetter(int index, double element) { // result list에 해당 index에 element 값을 넣어준다.
-        result.set(index, element);
+        results.set(index, element);
     }
 
     public int resultSize() { // result의 크기를 반환해준다.
-        return result.size();
+        return results.size();
     }
 
     public void removeResult() { // result list에서 첫번째 인덱스를 삭제한다.
-        result.removeFirst();
+        results.removeFirst();
     }
 
     public void inquiryResults() {
         System.out.print("[ ");
-        for (double d : result)
+        for (double d : results)
+            System.out.print(d + " ");
+        System.out.println("]");
+    }
+
+    public void calculateCircleArea(int radius) {
+        circleArea.add(radius * radius * PI);
+    }
+
+    public double circleGetter(int index) {
+        return circleArea.get(index);
+    }
+
+    public double circleGetLast() {
+        return circleArea.getLast();
+    }
+
+    public void circleSetter(int index, double element) {
+        circleArea.set(index, element);
+    }
+
+    public void inquiryCircle() {
+        System.out.print("[ ");
+        for (double d : circleArea)
             System.out.print(d + " ");
         System.out.println("]");
     }
 }
-
-
